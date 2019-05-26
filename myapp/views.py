debe8cae888d6c097b1756f444986f677c0bd943
hyojin <ahj2079@likelion.com>
2019-05-26 20:08:22 +0900

    first assignment

---
+++ myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def detail1(request):
+    return render(request, 'detail1.html')
+
+def detail2(request):
+    return render(request, 'detail2.html')
+
+def detail3(request):
+    return render(request, 'detail3.html')
